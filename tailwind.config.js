5eb02ca9836dabf55f28a19a65dcd55ea0ac8852
CHEGEBB <chegephil24@gmail.com>
2024-09-29 11:31:16 +0300

    fixing issues

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,82 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-
+    content: ["./App.{js,jsx,ts,tsx}", "./app/**/*.{js,jsx,ts,tsx}"],
     theme: {
         extend: {},
     },
     plugins: [],
-}
-plugins: [],
-}
-lugins: [],
-}
-lugins: [],
-}
-lugins: [],
-}
-lugins: [],
-}
-ugins: [],
-}
-lugins: [],
-}
-lugins: [],
-}
-lugins: [],
-}
-lugins: [],
-}
-ugins: [],
-}
-lugins: [],
-}
-lugins: [],
-}
-lugins: [],
-}
-ugins: [],
-}
-ugins: [],
-}
-ugins: [],
-}
-lugins: [],
-}
-lugins: [],
-}
-lugins: [],
-}
-lugins: [],
-}
-ugins: [],
-}
-lugins: [],
-}
-lugins: [],
-}
-lugins: [],
-}ugins: [],
-}
-lugins: [],
-}
-lugins: [],
-}
-lugins: [],
-}
-lugins: [],
-}ugins: [],
-}ugins: [],
-}ugins: [],
-}
-lugins: [],
-}
-lugins: [],
-}
-lugins: [],
-}
-lugins: [],
-}ugins: [],
-}lugins: [],
-}lugins: [],
-}lugins: [],
 }
