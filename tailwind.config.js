01c764147a1f77d11cc63fae473a05d03550c0f7
CHEGEBB <chegephil24@gmail.com>
2024-09-29 10:49:15 +0300

    new

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,3 +6,77 @@ module.exports = {
     },
     plugins: [],
 }
+plugins: [],
+}
+lugins: [],
+}
+lugins: [],
+}
+lugins: [],
+}
+lugins: [],
+}
+ugins: [],
+}
+lugins: [],
+}
+lugins: [],
+}
+lugins: [],
+}
+lugins: [],
+}
+ugins: [],
+}
+lugins: [],
+}
+lugins: [],
+}
+lugins: [],
+}
+ugins: [],
+}
+ugins: [],
+}
+ugins: [],
+}
+lugins: [],
+}
+lugins: [],
+}
+lugins: [],
+}
+lugins: [],
+}
+ugins: [],
+}
+lugins: [],
+}
+lugins: [],
+}
+lugins: [],
+}ugins: [],
+}
+lugins: [],
+}
+lugins: [],
+}
+lugins: [],
+}
+lugins: [],
+}ugins: [],
+}ugins: [],
+}ugins: [],
+}
+lugins: [],
+}
+lugins: [],
+}
+lugins: [],
+}
+lugins: [],
+}ugins: [],
+}lugins: [],
+}lugins: [],
+}lugins: [],
+}
