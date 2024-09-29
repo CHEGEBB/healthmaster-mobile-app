960733ed1900ca87afbec569d7664a95b9a38dd0
CHEGEBB <chegephil24@gmail.com>
2024-09-29 11:49:47 +0300

    Refactor app layout and dependencies
    - Refactor app_layout.jsx to use expo-router Stack component
    - Update app.json to include expo-font plugin
    - Remove unused _layout.tsx file
    - Add check.svg, arrow.svg, user.svg, email.svg, upload.svg, kidneys.svg, appointments.svg, cancelled.svg, and calendar.svg images
    - Update expo-font version in package.json
    - Add index.jsx file with initial content
    - Remove ThemedText-test.tsx and its snapshot
    - Remove ThemedView.tsx and TabBarIcon.tsx components
    - Add external link functionality to ExternalLink.tsx

---
+++ app/index.jsx
@@ -0,0 +1,16 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text className="">This is the Index screen.</Text>
+    </View>
+  );
+}
